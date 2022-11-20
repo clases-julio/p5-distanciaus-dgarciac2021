9d65aa1bf81a5bd6db6977e3668c435ec2553ad7
Diego Garcia Curras <d.garciac.2021@alumnos.urjc.es>
2022-11-20 17:49:39 +0100

    Editing done. Im trying on the RPI

--- usInstantaneo.py
+++ usInstantaneo.py
@@ -2,42 +2,47 @@
 import RPi.GPIO as GPIO
 import time
 
-try:
-      GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BCM)
 
-      PIN_TRIGGER = 21
-      PIN_ECHO = 20
-      PIN_R = 14
-      PIN_G = 15
-      PIN_B = 18
+PIN_TRIGGER = 21
+PIN_ECHO = 20
+PIN_R = 14
+PIN_G = 15
+PIN_B = 18
 
-      GPIO.setup(PIN_TRIGGER, GPIO.OUT)
-      GPIO.setup(PIN_R, GPIO.OUT)
-      GPIO.setup(PIN_G, GPIO.OUT)
-      GPIO.setup(PIN_B, GPIO.OUT)
-      GPIO.setup(PIN_ECHO, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(PIN_TRIGGER, GPIO.OUT)
+GPIO.setup(PIN_R, GPIO.OUT)
+GPIO.setup(PIN_G, GPIO.OUT)
+GPIO.setup(PIN_B, GPIO.OUT)
+GPIO.setup(PIN_ECHO, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-      GPIO.output(PIN_TRIGGER, GPIO.LOW)
-      GPIO.output(PIN_R, GPIO.LOW)
-      GPIO.output(PIN_G, GPIO.LOW)
-      GPIO.output(PIN_B, GPIO.LOW)
+GPIO.output(PIN_TRIGGER, GPIO.LOW)
+GPIO.output(PIN_R, GPIO.LOW)
+GPIO.output(PIN_G, GPIO.LOW)
+GPIO.output(PIN_B, GPIO.LOW)
 
-      print("Esperando a que se estabilice el US")
-      time.sleep(2)
+TRIGGER_TIME = 0.00001
 
-      GPIO.output(PIN_TRIGGER, GPIO.HIGH)
-      time.sleep(0.00001)
-      GPIO.output(PIN_TRIGGER, GPIO.LOW)
+def callbackExit(signal, frame): # signal and frame when the interrupt was executed.
+    GPIO.cleanup() # Clean GPIO resources before exit.
+    sys.exit(0)
 
-      while GPIO.input(PIN_ECHO)==0:
+def main():
+    while True:
+        GPIO.output(PIN_TRIGGER, GPIO.HIGH)
+        time.sleep(TRIGGER_TIME)
+        GPIO.output(PIN_TRIGGER, GPIO.LOW)
+
+        while GPIO.input(PIN_ECHO)==0:
             inicioPulso = time.time()
-      while GPIO.input(PIN_ECHO)==1:
+        while GPIO.input(PIN_ECHO)==1:
             finPulso = time.time()
 
-      duracionPulso = finPulso - inicioPulso
-      distancia = round(duracionPulso * 17150, 2)
-      print("Distancia: ", distancia, " cm")
+        duracionPulso = finPulso - inicioPulso
+        distancia = round(duracionPulso * 17150, 2)
+        print("Distancia: ", distancia, " cm", end="\r")
 
-finally:
-      GPIO.cleanup()
-      
+        signal.signal(signal.SIGINT, callbackExit) # callback for CTRL+C
+
+if __name__ == "__main__":
+    main()
