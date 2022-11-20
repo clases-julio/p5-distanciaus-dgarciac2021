fbac8e72ea14f75d07d8c2ea583859370a20550f
Diego Garcia Curras <d.garciac.2021@alumnos.urjc.es>
2022-11-20 20:10:25 +0100

    Editing done. Im trying on the RPI

--- usInstantaneo.py
+++ usInstantaneo.py
@@ -4,6 +4,8 @@ import time
 import signal
 import sys
 
+from Bluetin_Echo import Echo
+
 GPIO.setmode(GPIO.BCM)
 
 PIN_TRIGGER = 21
@@ -12,49 +14,32 @@ PIN_R = 14
 PIN_G = 15
 PIN_B = 18
 
-GPIO.setup(PIN_TRIGGER, GPIO.OUT)
+""" Calibrate sensor with initial speed of sound m/s value """
+SPEED_OF_SOUND = 343
+""" Initialise Sensor with pins, speed of sound. """
+echo = Echo(PIN_TRIGGER, PIN_ECHO, SPEED_OF_SOUND)
+
 GPIO.setup(PIN_R, GPIO.OUT)
 GPIO.setup(PIN_G, GPIO.OUT)
 GPIO.setup(PIN_B, GPIO.OUT)
-GPIO.setup(PIN_ECHO, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-GPIO.output(PIN_TRIGGER, GPIO.LOW)
 GPIO.output(PIN_R, GPIO.LOW)
 GPIO.output(PIN_G, GPIO.LOW)
 GPIO.output(PIN_B, GPIO.LOW)
 
-TRIGGER_TIME = 10000
-
 def callbackExit(signal, frame): # signal and frame when the interrupt was executed.
     GPIO.cleanup() # Clean GPIO resources before exit.
     sys.exit(0)
-
-def measure(measures = 5, echoPin = PIN_ECHO, triggerPin = PIN_TRIGGER):
-    measureSum = 0
-
-    for measue in range(0, measures): 
-        GPIO.output(PIN_TRIGGER, GPIO.HIGH)
-        time.nanosleep(TRIGGER_TIME)
-        GPIO.output(PIN_TRIGGER, GPIO.LOW)
-
-        while GPIO.input(PIN_ECHO)==0:
-            inicioPulso = time.time()
-        while GPIO.input(PIN_ECHO)==1:
-            finPulso = time.time()
-
-        measureSum = measureSum + ((finPulso - inicioPulso) * 17150)
-
-    return round(measureSum/measures, 2)
 
 def refreshData(data):
     print("                                        ", end="\r")
     print("Distancia: ", data, " cm", end="\r")
 
 def main():
-    while True:
-        refreshData(measure())
-
-        signal.signal(signal.SIGINT, callbackExit) # callback for CTRL+C
+    averageOf = 10
+    result = echo.read('cm', averageOf)
+    print('Average: {:.2f} mm'.format(result))
+    signal.signal(signal.SIGINT, callbackExit) # callback for CTRL+C
 
 if __name__ == "__main__":
     main()
