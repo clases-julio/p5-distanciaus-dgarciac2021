78a7b749cc84dac24e9bb0f91e10243685d2b7da
dgarcu <d.garciac.2021@alumnos.urjc.es>
2022-11-20 17:42:04 +0100

    Im editing on my PC. This is a torture.

--- usInstantaneo.py
+++ usInstantaneo.py
@@ -5,15 +5,24 @@ import time
 try:
       GPIO.setmode(GPIO.BCM)
 
-      PIN_TRIGGER = 4
-      PIN_ECHO = 17
+      PIN_TRIGGER = 21
+      PIN_ECHO = 20
+      PIN_R = 14
+      PIN_G = 15
+      PIN_B = 18
 
       GPIO.setup(PIN_TRIGGER, GPIO.OUT)
-      GPIO.setup(PIN_ECHO, GPIO.IN)
+      GPIO.setup(PIN_R, GPIO.OUT)
+      GPIO.setup(PIN_G, GPIO.OUT)
+      GPIO.setup(PIN_B, GPIO.OUT)
+      GPIO.setup(PIN_ECHO, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
       GPIO.output(PIN_TRIGGER, GPIO.LOW)
+      GPIO.output(PIN_R, GPIO.LOW)
+      GPIO.output(PIN_G, GPIO.LOW)
+      GPIO.output(PIN_B, GPIO.LOW)
 
-      print "Esperando a que se estabilice el US"
+      print("Esperando a que se estabilice el US")
       time.sleep(2)
 
       GPIO.output(PIN_TRIGGER, GPIO.HIGH)
@@ -27,7 +36,7 @@ try:
 
       duracionPulso = finPulso - inicioPulso
       distancia = round(duracionPulso * 17150, 2)
-      print "Distancia: ", distancia, " cm"
+      print("Distancia: ", distancia, " cm")
 
 finally:
       GPIO.cleanup()
